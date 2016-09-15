9b5b219f6e18dad936ae6d0bcc6a3c4d10c48bd4
dmschrispena <cpena@dmshouston.com>
2016-09-14 20:24:26 -0500

    Fixed incorrect file path

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.get('/api/customers', customersController.get);
 app.use(middleware);
 app.use(webpackHotMiddleware(compiler));
 app.get('*', function response(req, res) {
-  res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+  res.write(middleware.fileSystem.readFileSync(path.join(__dirname, '../dist/index.html')));
   res.end();
 });
 
